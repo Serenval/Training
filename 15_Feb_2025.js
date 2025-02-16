623a0bca8577ddbbdf03d12507b92fab65f401fd
Bohdan <telanim@outlook.com>
2025-02-16 19:10:50 +0200

    Testing Immutable and Mutable

---
+++ 15_Feb_2025.js
@@ -0,0 +1,13 @@
+/*
+* Immutable Primitive Values and Mutable Object References
+*/
+
+let s = 'Hello';
+console.log(s.toUpperCase());
+console.log(s);
+
+let obj = {x:1};
+obj.b = 3;
+obj.x = 2;
+
+console.log(obj);
