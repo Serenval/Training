0d753c30067135644d723362419665ab3cc15926
Bohdan <telanim@outlook.com>
2025-04-16 21:28:22 +0300

    Polish word replacer

---
+++ code_wars_riddles/Polish alphabet.js
@@ -0,0 +1,30 @@
+function correctPolishLetters (string) {
+  const correctedString = string.split('').map(char => {
+    switch(char) {
+      case 'ą':
+        return 'a';
+      case 'ć':
+        return 'c';
+      case 'ę':
+        return 'e';
+      case 'ł':
+        return 'l';
+      case 'ń':
+        return 'n';
+      case 'ó':
+        return 'o';
+      case 'ś':
+        return 's';
+      case 'ź':
+        return 'z';
+      case 'ż':
+        return 'z';
+      default:
+        return char;
+    }
+  });
+  return correctedString.join('');
+}
+
+console.log(correctPolishLetters('Błądziński'));
+
